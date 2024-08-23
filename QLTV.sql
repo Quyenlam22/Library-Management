a74cac9a0dee2bfee757b93362e18f1b8fb953aa
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2024-08-23 15:52:01 +0700

    Update project the second time

--- QLTV.sql
+++ QLTV.sql
@@ -18,6 +18,9 @@ CREATE TABLE SACH(
 	Nha_xb nvarchar(30),
 	So_luong int
 )
+INSERT INTO SACH VALUES
+('MS01', N'Dế mèn phiêu lưu ký', N'Tô Hoài', N'Bộ Giáo dục', 100),
+('MS02', N'Rùa và thỏ', 'Jean de La Fontaine', N'NXB Văn học', 100);
 
 --Tạo bảng độc giả
 CREATE TABLE KHACH_HANG(
@@ -26,6 +29,9 @@ CREATE TABLE KHACH_HANG(
 	Phone varchar(10),
 	Email varchar(20)
 )
+INSERT INTO KHACH_HANG VALUES
+('KH01', N'Lê Văn A', '0123456789', 'levana@gmail.com'),
+('KH02', N'Nguyễn Thị B', '0987654321', 'nguyenthib@gmail.com');
 
 --Tạo bảng phiếu mượn
 CREATE TABLE Phieu_Muon(
@@ -38,3 +44,7 @@ So_luong int,
 FOREIGN KEY (Ma_Khach_Hang) REFERENCES KHACH_HANG(Ma_Khach_Hang),
 FOREIGN KEY (Ma_Sach) REFERENCES SACH(Ma_Sach)
 )
+INSERT INTO Phieu_Muon VALUES
+('PM01', 'KH02', 'MS01', 2024-3-3, 2024-26-3, 1),
+('PM02', 'KH01', 'MS02', 2024-15-5, 2024-26-5, 1);
+
